3e82264ef9d8ac030ad9549c76563894dcbf50f6
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-09-01 16:01:47 +0200

    test with one url

--- todo_list_backend/settings/production.py
+++ todo_list_backend/settings/production.py
@@ -10,7 +10,7 @@ from .base import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ##ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS = ['todo-list-backend-ljex.onrender.com', '.onrender.com']
+ALLOWED_HOSTS = ['todo-list-backend-ljex.onrender.com']
 
 
 # ==============================================================================
