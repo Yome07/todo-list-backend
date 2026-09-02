59b299750bfa616daa979431cbb115bcebd9e32b
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-09-02 16:34:05 +0200

    remove / static

--- todo_list_backend/settings/base.py
+++ todo_list_backend/settings/base.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", # L'origine de votre application React en développement
